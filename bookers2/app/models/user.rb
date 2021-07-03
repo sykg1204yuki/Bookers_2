57027a1c79693f4e0d274cb77314e25939f58b92
sykg1204yuki <y.yuki040507@gmail.com>
2021-07-03 01:46:43 +0000

    応用課題4完成【フォロー・フォロワー機能】

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -8,6 +8,36 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   
+  # ここからフォロー・フォロワー機能のアソシエーション
+  has_many :reverse_of_relationships, class_name:"Relationship",foreign_key:"followed_id", dependent: :destroy
+  
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+   # 被フォロー関係を通じて参照→followed_idをフォローしている人
+  
+  has_many :relationships, class_name:"Relationship",foreign_key:"follower_id", dependent: :destroy
+   # 【class_name:"Relationship"】は省略可能
+   
+  has_many :followings, through: :relationships, source: :followed
+   # 与フォロー関係を通じて参照→follower_idをフォローしている人
+   
+  # フォロー・フォロワーアソシエーション終了
+  # ここからフォロー・フォロワー機能メソッド
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+    
+  end 
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+    
+  end 
+  
+  def following?(user)
+    followings.include?(user)
+    
+  end 
+  # フォロー・フォロワー機能メソッド終了
+  
   attachment :profile_image
 
   validates :name, uniqueness: true, length: { in: 2..20 }
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
-  
-  resources :users, only: [:index, :create, :show, :edit, :update]
-  resources :books do 
+
+  resources :users, only: [:index, :create, :show, :edit, :update] do
+    # フォロー・フォロワー機能ルーティング
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as:'followings'
+    get 'followers' => 'relationships#followers', as:'followers'
+
+  end
+
+  resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
-    
-  end 
+
+  end
 
 end
