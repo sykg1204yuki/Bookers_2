bc34efcd913df1a3468c87c031e0879e9151afd0
sykg1204yuki <y.yuki040507@gmail.com>
2021-07-06 14:11:34 +0000

    いいね・コメント機能の非同期通信

--- bookers2/app/assets/javascripts/application.js
+++ bookers2/app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require popper
 //= require bootstrap-sprockets
 
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- bookers2/app/controllers/post_comments_controller.rb
+++ bookers2/app/controllers/post_comments_controller.rb
@@ -1,19 +1,22 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
     # ↑ comment = PostComment.new(post_comment_params)
     #   comment.user_id = current_user.id. の省略
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+
+
   end
+
+
 
   def destroy
     PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     redirect_back(fallback_location: root_path)
-    
+
   end
 
   private
